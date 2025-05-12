e966343a1deed2ee38624e8a9e7da6d8a5c3b5c6
Nazariy <nazariy.holovach2007@gmail.com>
2025-05-12 23:24:11 +0300

    Initial commit

---
+++ frontend/app.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h1>hello world</h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+</script>
---
+++ frontend/postcss.config.cjs
@@ -0,0 +1,35 @@
+const purgecss = require('@fullhuman/postcss-purgecss').default;
+
+module.exports = {
+    plugins: [
+        require('postcss-preset-env')(),
+        purgecss({
+            content: [
+                './app.vue',               // Reference app.vue in Nuxt 3 setup
+                './static/**/*.html',      // Static HTML files if they exist
+                './nuxt.config.ts',        // Include Nuxt config if needed
+                './pages/**/*.vue',        // Wildcard for future .vue files in pages
+                './components/**/*.vue',   // Wildcard for future .vue files in components
+                './layouts/**/*.vue',      // Wildcard for future .vue files in layouts
+            ],
+            safelist: [
+                'safelist-class',
+                'active',
+                'collapsed',
+                'show',
+                'in-view',
+                'removing',
+                '@slideInFadeRight',
+                '@slideInFadeLeft',
+                '@slideInFadeUp',
+                '@slideInFadeDown',
+                '@fade',
+                '@bounce',
+            ],
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+            keyframes: false,
+            fontFace: true,
+            whitelistPatterns: [/lumecss/],
+        }),
+    ],
+};
