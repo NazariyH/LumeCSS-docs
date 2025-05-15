cffa8a03e6425ae41bc74a3b6ca067ea371ecf66
Nazariy <nazariy.holovach2007@gmail.com>
2025-05-15 05:42:42 +0300

    Updated postcss.config.cjs

--- frontend/postcss.config.cjs
+++ frontend/postcss.config.cjs
@@ -19,6 +19,8 @@ module.exports = {
                 'show',
                 'in-view',
                 'removing',
+                'show-password',
+                'hide-password',
                 '@slideInFadeRight',
                 '@slideInFadeLeft',
                 '@slideInFadeUp',
