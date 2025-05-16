fa15eeb66de5530b8abbfea50fcd4b139fc52321
Nazariy <nazariy.holovach2007@gmail.com>
2025-05-16 05:48:29 +0300

    Added prism.js plugin

---
+++ frontend/plugins/prism.client.ts
@@ -0,0 +1,15 @@
+// plugins/prism.client.ts
+import Prism from 'prismjs'
+import 'prismjs/themes/prism-tomorrow.css' // Dark theme
+
+// Load language components
+import 'prismjs/components/prism-javascript'
+// Optional: more languages like typescript, bash, json etc.
+
+export default defineNuxtPlugin(() => {
+    return {
+        provide: {
+            prism: Prism
+        }
+    }
+})
