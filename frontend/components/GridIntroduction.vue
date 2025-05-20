21933f62e3720d56dedd5d039f044d2e523a64b3
Nazariy <nazariy.holovach2007@gmail.com>
2025-05-21 01:09:44 +0300

    Updated the grid introduction component

--- frontend/components/GridIntroduction.vue
+++ frontend/components/GridIntroduction.vue
@@ -18,27 +18,52 @@
             <div class="w-40 row gap-1">
                 <div class="col-4-xl col-4-lg col-4-md col-12-sm col-12-xs relative min-h-12">
                     <div class="w-full h-full overflow-hidden xl:rounded-l-lg lg:rounded-l-lg md:rounded-l-lg
-                                sm:rounded-t-lg xs:rounded-t-lg bg-center bg-cover bg-no-repeat grid-img-1">
-
+                                sm:rounded-t-lg xs:rounded-t-lg">
+                        <div class="w-full h-full bg-center bg-cover bg-no-repeat grid-img-1 duration-700 ease-in
+                                    hover:scale-1.2">
+                            <div class="w-full h-full hover:bg-dark hover:trans-70-bg hover:backdrop-blur-xs duration-300
+                                        ease flex justify-center items-center text-transparent hover:text-light">
+                                col-4
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="col-8-xl col-8-lg col-8-md col-12-sm col-12-xs flex flex-column pr-0">
                     <div class="w-full h-full relative row w-full gap-1">
                         <div class="col-6 h-12 mt-0 pt-0 relative">
-                            <div class="w-full h-full overflow-hidden bg-center bg-cover bg-no-repeat grid-img-2">
-
+                            <div class="w-full h-full overflow-hidden">
+                                <div class="w-full h-full bg-center bg-cover bg-no-repeat grid-img-2 duration-700 ease-in
+                                            hover:scale-1.2">
+                                    <div class="w-full h-full hover:bg-dark hover:trans-70-bg hover:backdrop-blur-xs
+                                                duration-300 ease flex justify-center items-center text-transparent
+                                                hover:text-light">
+                                        col-6
+                                    </div>
+                                </div>
                             </div>
                         </div>
                         <div class="col-6 h-12 mt-0 pt-0 pr-0 relative">
-                            <div class="w-full h-full overflow-hidden md:rounded-se-lg lg:rounded-se-lg xl:rounded-se-lg
-                                        bg-center bg-cover bg-no-repeat grid-img-3">
-
+                            <div class="w-full h-full overflow-hidden md:rounded-se-lg lg:rounded-se-lg xl:rounded-se-lg">
+                                <div class="w-full h-full bg-center bg-cover bg-no-repeat grid-img-3 duration-700 ease-in
+                                            hover:scale-1.2">
+                                    <div class="w-full h-full hover:bg-dark hover:trans-70-bg hover:backdrop-blur-xs
+                                                duration-300 ease flex justify-center items-center text-transparent
+                                                hover:text-light">
+                                        col-6
+                                    </div>
+                                </div>
                             </div>
                         </div>
                         <div class="col-12 h-12 mb-0 pb-0 pr-0 relative">
-                            <div class="w-full h-full overflow-hidden rounded-ee-lg sm:rounded-es-lg xs:rounded-es-lg
-                                        bg-center bg-cover bg-no-repeat grid-img-4">
-
+                            <div class="w-full h-full overflow-hidden rounded-ee-lg sm:rounded-es-lg xs:rounded-es-lg">
+                                <div class="w-full h-full bg-center bg-cover bg-no-repeat grid-img-4 duration-700 ease-in
+                                            hover:scale-1.2">
+                                    <div class="w-full h-full hover:bg-dark hover:trans-70-bg hover:backdrop-blur-xs
+                                                duration-300 ease flex justify-center items-center text-transparent
+                                                hover:text-light">
+                                        col-12
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -51,17 +76,43 @@
 <style lang="scss">
     .grid-img-1 {
         background-image: url("/images/grid-1-img.jpg");
+        background-size: cover !important;
+        background-position: center !important;
+
+        & > div {
+            transition: background-color 1000ms ease !important;
+        }
     }
 
     .grid-img-2 {
         background-image: url("/images/grid-2-img.jpg");
+        background-size: cover !important;
+        background-position: center !important;
+
+        & > div {
+            transition: background-color 1000ms ease !important;
+        }
     }
 
     .grid-img-3 {
         background-image: url("/images/grid-3-img.jpg");
+        background-size: cover !important;
+        background-position: center !important;
+
+        & > div {
+            transition: background-color 1000ms ease !important;
+        }
     }
 
     .grid-img-4 {
         background-image: url("/images/grid-4-img.jpg");
+        background-size: cover !important;
+        background-position: center !important;
+
+        & > div {
+            transition: background-color 1000ms ease !important;
+        }
     }
 </style>
+<script setup lang="ts">
+</script>
